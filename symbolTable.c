3aeece1f2123bdcb7a0bcf907b3c2152ac6842c6
Balawal Sultan <bsultan@unibz.it>
2021-05-25 19:31:37 +0200

    solved symbolTable compile errors

--- symbolTable.c
+++ symbolTable.c
@@ -5,51 +5,52 @@
 #include <stdbool.h>
 
 
-struct symbol{
+typedef struct symbol{
     char* name;
     int type, value;
-    symbol* next;
-};
+    struct symbol* next;
+} symbol;
 
-struct symbolHT{
-    symbol head, tail;
-}
+typedef struct symbolHT{
+    symbol* head;
+    symbol* tail;
+}symbolHT;
 
 symbolHT symbolTable;
 
 symbol lookup(char* name){
-    symbol temp = symbolTable.head;
+    symbol* temp = symbolTable.head;
     
-    while(temp != null){
-        if(strcmp(temp.name, name) == 0)
-            return temp;
+    while(temp != NULL){
+        if(strcmp(temp->name, name) == 0)
+            return *temp;
         
-        temp = temp.next;
+        temp = temp->next;
     }
 
-    return temp;
+    return *temp;
 }
 
-symbol createSymbol(char* name, int type, int value){
-    symbol temp;
-    temp.name = name;
-    temp.type = type;
-    temp.value = value;
+symbol* createSymbol(char* name, int type, int value){
+    symbol* temp;
+    temp->name = name;
+    temp->type = type;
+    temp->value = value;
     return temp;
 }
 
 void addToTail(char* name, int type, int value){
-    symbol newSymbol = createSymbol(name, type, value);
+    symbol* newSymbol = createSymbol(name, type, value);
 
-    if(symbolTable.head != null){ //is not empty
-        symbolTable.tail.next = newSymbol;
+    if(symbolTable.head != NULL){ //is not empty
+        symbolTable.tail->next = newSymbol;
 
     }else if(symbolTable.head == symbolTable.tail){
-        symbolTable.head.next = newSymbol;
-        symbolTable.tail = symbolTable.head.next;
+        symbolTable.head->next = newSymbol;
+        symbolTable.tail = symbolTable.head->next;
 
     }else{
-        symbolTable.tail.next = newSymbol;
-        symbolTable.tail = symbolTable.tail.next;
+        symbolTable.tail->next = newSymbol;
+        symbolTable.tail = symbolTable.tail->next;
     }
 }
